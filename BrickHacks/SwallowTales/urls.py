a134a4b8b587d63b06093c5d4bb2e5f93ed38599
Victoria Sardelli <vlsardelli@outlook.com>
2016-03-05 22:44:41 -0500

    made small addition to urls.py for sign up page

--- BrickHacks/SwallowTales/urls.py
+++ BrickHacks/SwallowTales/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.startPage, name='home'),
     url(r'^login/', views.login_view, name='login'),
     url(r'^logout/', views.logout_view, name='logout'),
+    url(r'^sign_up/', views.sign_up_view, name='sign_up'),
     ]
