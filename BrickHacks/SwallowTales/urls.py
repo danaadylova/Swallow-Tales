9c6d5945dcb59da16015298a0d3b0f777beb495d
Victoria Sardelli <vlsardelli@outlook.com>
2016-03-05 23:29:24 -0500

    fixed merge conflict

--- BrickHacks/SwallowTales/urls.py
+++ BrickHacks/SwallowTales/urls.py
@@ -8,9 +8,5 @@ urlpatterns = [
     url(r'^$', views.startPage, name='home'),
     url(r'^login/', views.login_view, name='login'),
     url(r'^logout/', views.logout_view, name='logout'),
-<<<<<<< Updated upstream
-#    url(r'^sign_up/', views.sign_up_view, name='sign_up'),
-=======
     url(r'^signup/', views.signup_view, name='signup'),
->>>>>>> Stashed changes
     ]
